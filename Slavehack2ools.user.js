7fd81e718d8139f766a761d3e9cfd40028fa9497
mcdfinn <professedhate@gmail.com>
2017-07-10 11:25:02 -0400

    added script

---
+++ Slavehack2ools.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         slavehack2ools
+// @namespace    https://github.com/mcdfinn/slavehack2ools
+// @version      0.1
+// @description  slavehack utils
+// @author       professedhate
+// @include      https://www.slavehack2.com/*
+// @match        http://tampermonkey.net/?ext=dhdg&browser=chrome
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Your code here...
+})();
